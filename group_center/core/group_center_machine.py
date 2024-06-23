e96afd151c475225071b1bfa5fb5c0dc46d7751b
Haomin Kong <a645162@qq.com>
2024-06-23 22:19:41 +0800

    Remote user configure

--- group_center/core/group_center_machine.py
+++ group_center/core/group_center_machine.py
@@ -1,5 +1,5 @@
 import json
-from typing import Tuple
+from typing import Tuple, Optional
 
 import loguru
 import requests
@@ -20,7 +20,7 @@ group_center_public_part: dict = {
     "serverNameEng": MACHINE_NAME_SHORT,
 }
 
-logger: loguru.logger = None
+logger: Optional[loguru.logger] = None
 
 
 def init_logger():
@@ -54,7 +54,23 @@ def set_machine_password(password: str):
     MACHINE_PASSWORD = password
 
 
+def __init_from_env(skip_if_exist: bool = True):
+    if skip_if_exist and GROUP_CENTER_URL != "":
+        return
+
+    env_machine_config: Tuple[str, str, str, str] = get_env_machine_config()
+
+    set_group_center_host(env_machine_config[0])
+    set_server_name(env_machine_config[1])
+    set_server_name_short(env_machine_config[2])
+    set_machine_password(env_machine_config[3])
+
+
 def group_center_get_url(target_api: str):
+    __init_from_env()
+
+    global GROUP_CENTER_URL
+
     if GROUP_CENTER_URL.endswith("/"):
         if target_api.startswith("/"):
             target_api = target_api[1:]
@@ -122,12 +138,7 @@ def __group_center_login(username: str, password: str) -> bool:
 
 
 def group_center_login() -> bool:
-    if GROUP_CENTER_URL == "":
-        env_machine_config: Tuple[str, str, str,str] = get_env_machine_config()
-        set_group_center_host(env_machine_config[0])
-        set_server_name(env_machine_config[1])
-        set_server_name_short(env_machine_config[2])
-        set_machine_password(env_machine_config[3])
+    __init_from_env()
 
     return __group_center_login(
         username=MACHINE_NAME_SHORT, password=MACHINE_PASSWORD
