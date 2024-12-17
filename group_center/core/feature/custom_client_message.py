e239081f8466635a5bb0e9dd3990ac3f3a599807
Haomin Kong <a645162@qq.com>
2024-12-17 15:52:35 +0800

    feat(MachineMessage): Support custom At

--- group_center/core/feature/custom_client_message.py
+++ group_center/core/feature/custom_client_message.py
@@ -2,14 +2,13 @@ from group_center.core.feature.machine_message import new_message_enqueue
 
 
 def machine_message_directly(
-    machine_name: str,
-    machine_name_eng: str,
-    content: str,
+    server_name: str, server_name_eng: str, content: str, at: str = ""
 ):
     data_dict: dict = {
-        "serverName": machine_name,
-        "serverNameEng": machine_name_eng,
+        "serverName": server_name,
+        "serverNameEng": server_name_eng,
         "content": content,
+        "at": at,
     }
 
     new_message_enqueue(data_dict, "/api/client/machine/message")
@@ -28,5 +27,13 @@ def machine_user_message_directly(
 
 
 if __name__ == "__main__":
-    machine_message_directly("3090", "3090", "Test group message")
-    machine_user_message_directly("konghaomin", "Test personal message")
+    machine_message_directly(
+        server_name="3090",
+        server_name_eng="3090",
+        content="Test group message",
+        at="孔昊旻",
+    )
+
+    machine_user_message_directly(
+        user_name="konghaomin", content="Test personal message"
+    )
